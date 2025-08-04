55aeb83766da617c533d85a34c195860190157ab
Syed Rahmath <syedrahmath102@gmail.com>
2025-08-04 20:34:06 +0530

    added some modification

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,22 +1,37 @@
 pipeline {
     agent any
 
+    environment {
+        IMAGE_NAME = "yourdockerhubusername/your-image-name"
+    }
+
     stages {
         stage('Clone Repo') {
             steps {
-                echo 'Cloning your repo...'
+                echo "Cloning your repo..."
                 git 'https://github.com/SYED6281/local.git'
             }
         }
-        stage('Build') {
+
+        stage('Build Docker Image') {
             steps {
-                echo 'This is the Build stage.'
+                sh 'docker build -t $IMAGE_NAME .'
             }
         }
-        stage('Test') {
+
+        stage('Run in Kubernetes') {
             steps {
-                echo 'Running some basic tests...'
-                sh 'echo Hello from Jenkins!'
+                sh '''
+                kubectl delete deployment website-deploy --ignore-not-found
+                kubectl create deployment website-deploy --image=$IMAGE_NAME
+                kubectl expose deployment website-deploy --type=NodePort --port=80
+                '''
+            }
+        }
+
+        stage('Show URL') {
+            steps {
+                sh 'minikube service website-deploy --url'
             }
         }
     }
